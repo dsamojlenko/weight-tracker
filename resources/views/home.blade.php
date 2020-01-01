f8b89ccf391f7aae6609889dd3777f52263298b3
Dave Samojlenko <dsamojlenko@gmail.com>
2020-01-01 15:57:17 -0500

    allow removing overlay

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,12 +14,15 @@
     <body>
         <div id="app">
             @if ($message = Session::get('error'))
-                <div class="absolute top-0 left-0 w-full h-screen z-10 bg-gray-100 opacity-75"></div>
+                <div class="error-modal">
+                    <div class="absolute top-0 left-0 w-full h-screen z-20 flex items-center justify-center">
 
-                <div class="absolute top-0 left-0 w-full h-screen z-10 flex items-center justify-center">
-                    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded m-4 z-50" role="alert">
-                        <strong class="font-bold">Nice try, fatty!</strong>
-                        <span class="block sm:inline">{{ $message }}</span>
+                        <div class="error-overlay absolute top-0 left-0 w-full h-screen z-10 bg-gray-100 opacity-75"></div>
+
+                        <div class="z-20 bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded m-4" role="alert">
+                            <strong class="font-bold">Nice try, fatty!</strong>
+                            <span class="block sm:inline">{{ $message }}</span>
+                        </div>
                     </div>
                 </div>
             @endif
@@ -125,5 +128,16 @@
             </div>
         </div>
         <script src="{{ mix('js/app.js') }}"></script>
+
+        <script type="text/javascript">
+            const overlay = document.querySelector('.error-overlay');
+            const modal = document.querySelector('.error-modal');
+            if(overlay) {
+                overlay.addEventListener('click', function () {
+                    overlay.remove()
+                    modal.remove()
+                })
+            }
+        </script>
     </body>
 </html>
