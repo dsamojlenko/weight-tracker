2d121221c22bac1a3535b6b9fe593f458739f8c1
Dave Samojlenko <dsamojlenko@gmail.com>
2020-01-01 18:29:19 -0500

    remove title

--- resources/views/chart_panel.blade.php
+++ resources/views/chart_panel.blade.php
@@ -1,5 +1,3 @@
 <div class="w-full sm:hidden md:block md:w-1/3 xl:w-2/3 md:p-4 md:h-full p-4">
-    <h2 class="text-2xl mb-4">The Chart</h2>
-
     <progress-chart :datasets="{{ $data }}" :labels="{{ $labels }}"></progress-chart>
 </div>
