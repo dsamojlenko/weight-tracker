1e3f9e23121f8b54e686ae8b7dae3f0652dea0b6
Dave Samojlenko <dsamojlenko@gmail.com>
2020-01-01 15:30:39 -0500

    make it accept only numbers

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,7 +38,7 @@
                         @csrf
                         <input type="hidden" name="user_id" value="2">
                         <div class="relative">
-                            <input type="text" name="weight" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <input type="number" name="weight" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                             <button type="submit" class="absolute right-0 top-0 p-2"><span class="sr-only">Submit</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="h-6 w-6"><path d="M10 .4C4.697.4.399 4.698.399 10A9.6 9.6 0 0 0 10 19.601c5.301 0 9.6-4.298 9.6-9.601 0-5.302-4.299-9.6-9.6-9.6zm-.001 17.2a7.6 7.6 0 1 1 0-15.2 7.6 7.6 0 1 1 0 15.2zM10 8H6v4h4v2.5l4.5-4.5L10 5.5V8z"/></svg>
                             </button>
@@ -85,7 +85,7 @@
                         @csrf
                         <input type="hidden" name="user_id" value="1">
                         <div class="relative">
-                            <input type="text" name="weight" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+                            <input type="number" name="weight" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
                             <button type="submit" class="absolute right-0 top-0 p-2"><span class="sr-only">Submit</span>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="h-6 w-6"><path d="M10 .4C4.697.4.399 4.698.399 10A9.6 9.6 0 0 0 10 19.601c5.301 0 9.6-4.298 9.6-9.601 0-5.302-4.299-9.6-9.6-9.6zm-.001 17.2a7.6 7.6 0 1 1 0-15.2 7.6 7.6 0 1 1 0 15.2zM10 8H6v4h4v2.5l4.5-4.5L10 5.5V8z"/></svg>
                             </button>
